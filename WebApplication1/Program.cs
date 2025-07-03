9c4c36d395f06b6d58f4574306cc01b6c9f219e7
Aleksandr Khokhlachev <aleksandr.hohlatsov@gmail.com>
2025-07-03 10:26:50 +0300

    pass handlers to endpoint

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -10,13 +10,64 @@ var app = builder.Build();
 //     app.UseHttpLogging();
 // }
 
+// if (!app.Environment.IsDevelopment())
+// {
+//     app.UseExceptionHandler("/error");
+// }
+
 app.UseWelcomePage("/");
+// app.UseDeveloperExceptionPage();
 app.UseStaticFiles();
 app.UseRouting();
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/person", () => new Person("Andrew", "Lock"));
+app.MapGet("/error", () => "Sorry, an error occurred");
+
+var people = new List<Person>
+{
+    new("Tom", "Hanks"),
+    new("Denzel", "Washington"),
+    new("Leondardo", "DiCaprio"),
+    new("Al", "Pacino"),
+    new("Morgan", "Freeman"),
+};
+app.MapGet("/person/{name}", (string name) => people.Where(p => p.FirstName.StartsWith(name)));
+
+app.MapGet("/fruit", () => Fruit.All);
+
+var getFruit = (string id) => Fruit.All[id];
+app.MapGet("/fruit/{id}", getFruit);
+
+app.MapPost("/fruit/{id}", Handlers.AddFruit);
+
+Handlers handlers = new();
+app.MapPut("/fruit/{id}", handlers.ReplaceFruit);
+
+app.MapDelete("/fruit/{id}", DeleteFruit);
 
 app.Run();
+
+void DeleteFruit(string id)
+{
+    Fruit.All.Remove(id);
+}
+
+record Fruit(string Name, int Stock)
+{
+    public static readonly Dictionary<string, Fruit> All = new();
+};
+
+class Handlers
+{
+    public void ReplaceFruit(string id, Fruit fruit)
+    {
+        Fruit.All[id] = fruit;
+    }
+
+    public static void AddFruit(string id, Fruit fruit)
+    {
+        Fruit.All.Add(id, fruit);
+    }
+}
 
 public record Person(string FirstName, string LastName);
